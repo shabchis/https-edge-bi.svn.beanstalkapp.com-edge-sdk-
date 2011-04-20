ad0e8bfb1cc0ad8acee3a6834588d747e913e464
doronassayas <doronassayas@a563df95-700e-4ba9-babd-a50dda716297>
2011-04-20 11:19:24 +0000

    Adding SDK tester

---
+++ Applications/Edge.SDK.ServiceTester/trunk/xaml/App.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Reflection;
+using System.IO;
+using Edge.Core.Configuration;
+using System.ComponentModel;
+using Edge.Core.Services;
+using System.Collections.ObjectModel;
+
+namespace Edge.SDK.ServiceTester
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		public static BindingData BindingData = new BindingData();
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			// Get an alternate file name
+			string configFileName = EdgeServicesConfiguration.DefaultFileName;
+			if (e.Args.Length > 0 && e.Args[0].StartsWith("/") && e.Args[0].Length > 1)
+			{
+				configFileName = e.Args[0].Substring(1);
+			}
+
+			#if !DEBUG // change to !DEBUG
+			try
+			{
+				EdgeServicesConfiguration.Load(configFileName);
+			}
+			catch (Exception ex)
+			{
+				
+				MessageBox.Show
+				(
+					messageBoxText:	String.Format("Error loading the configuration file {0}\n\n({1}: {2})",
+						Path.Combine(Directory.GetCurrentDirectory(), configFileName),
+						ex.GetType().Name,
+						ex.Message),
+					caption: "Error",
+					button: MessageBoxButton.OK,
+					icon: MessageBoxImage.Error
+				);
+				Shutdown();
+				return;
+			}
+			#else
+			EdgeServicesConfiguration.Load(configFileName);
+			#endif
+
+			BindingData.Services = new ObservableCollection<ServiceDisplayInfo>();
+			foreach (ServiceElement serviceConfig in EdgeServicesConfiguration.Current.Services)
+			{
+				if (serviceConfig.IsPublic)
+					BindingData.Services.Add(new ServiceDisplayInfo(serviceConfig));
+			}
+
+		}
+	}
+
+	public class BindingData
+	{
+		public ObservableCollection<ServiceDisplayInfo> Services { get; set; }
+	}
+
+	
+}
---
+++ Applications/Edge.SDK.ServiceTester/trunk/xaml/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Edge.SDK.ServiceTester
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.DataContext = App.BindingData;
+		}
+
+		private void _Toolbar_Start_Click(object sender, RoutedEventArgs e)
+		{
+			var service = _Tree.SelectedItem as ServiceDisplayInfo;
+			if (service == null)
+				return;
+
+			service.Start();
+		}
+	}
+}
