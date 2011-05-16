c27e2c187654f0d4cd955d2dae3d7d5beb0471d1
doronassayas <doronassayas@a563df95-700e-4ba9-babd-a50dda716297>
2011-05-16 10:19:30 +0000

    Branching tester for Services v2 (core v3)

---
+++ Applications/Edge.SDK.ServiceTester/branches/v3/App.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using Edge.Core.Configuration;
+using Edge.Core.Services;
+using Edge.Data.Pipeline;
+
+namespace Edge.SDK.ServiceTester
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		public static BindingData BindingData = new BindingData();
+		public static DeliveryDBServer DeliveryServer = new DeliveryDBServer();
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			// Get an alternate file name
+			string configFileName = EdgeServicesConfiguration.DefaultFileName;
+			if (e.Args.Length > 0 && e.Args[0].StartsWith("/") && e.Args[0].Length > 1)
+			{
+				configFileName = e.Args[0].Substring(1);
+			}
+
+			#if !DEBUG // change to !DEBUG
+			try
+			{
+				EdgeServicesConfiguration.Load(configFileName);
+			}
+			catch (Exception ex)
+			{
+				
+				MessageBox.Show
+				(
+					messageBoxText:	String.Format("Error loading the configuration file {0}\n\n({1}: {2})",
+						Path.Combine(Directory.GetCurrentDirectory(), configFileName),
+						ex.GetType().Name,
+						ex.Message),
+					caption: "Error",
+					button: MessageBoxButton.OK,
+					icon: MessageBoxImage.Error
+				);
+				Shutdown();
+				return;
+			}
+			#else
+			EdgeServicesConfiguration.Load(configFileName);
+			#endif
+
+			BindingData.Services = new ObservableCollection<ServiceDisplayInfo>();
+			foreach (ServiceElement serviceConfig in EdgeServicesConfiguration.Current.Services)
+			{
+				if (serviceConfig.IsPublic)
+					BindingData.Services.Add(new ServiceDisplayInfo(serviceConfig));
+			}
+
+			DeliveryServer.Start();
+
+			// Auto start if there is only a single service
+			if (BindingData.Services.Count == 1)
+				BindingData.Services[0].Start();
+		}
+
+		protected override void OnExit(ExitEventArgs e)
+		{
+			base.OnExit(e);
+			DeliveryServer.Stop();
+		}
+	}
+
+	public class BindingData
+	{
+		public ObservableCollection<ServiceDisplayInfo> Services { get; set; }
+	}
+
+	
+}
---
+++ Applications/Edge.SDK.ServiceTester/branches/v3/classes/ServiceDisplayInfo.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Core.Services;
+using System.ComponentModel;
+using Edge.Core.Configuration;
+using System.Collections.ObjectModel;
+
+namespace Edge.SDK.ServiceTester
+{
+	public class ServiceDisplayInfo:INotifyPropertyChanged
+	{
+		#region props
+		// ...................
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public ServiceInstance Instance
+		{
+			get;
+			private set;
+		}
+
+		public ServiceElement Configuration
+		{
+			get;
+			private set;
+		}
+
+		public long? InstanceID
+		{
+			get;
+			private set;
+		}
+
+		public string Name
+		{
+			get;
+			private set;
+		}
+
+		public bool Unplanned
+		{
+			get;
+			private set;
+		}
+
+		public ServiceDisplayInfo Parent
+		{
+			get;
+			private set;
+		}
+
+		public double Progress
+		{
+			get { return Instance != null ? Instance.Progress*100 : 0; }
+		}
+
+		string _customStatus;
+
+		public string Status
+		{
+			get
+			{
+				if (_customStatus != null)
+					return _customStatus;
+				else if (Instance == null)
+					return string.Empty;
+				else
+					return Instance.State == ServiceState.Ended ?
+						Instance.Outcome.ToString() :
+						Instance.State.ToString();
+			}
+		}
+
+		public ObservableCollection<ServiceDisplayInfo> Children
+		{
+			get;
+			private set;
+		}
+
+		// ...................
+		#endregion
+
+		#region ctors
+		// ...................
+
+		public ServiceDisplayInfo(ServiceElement configuration, string name = null)
+		{
+			Configuration = configuration;
+			Name = name ?? Configuration.Name;
+
+			Children = new ObservableCollection<ServiceDisplayInfo>();
+			if (Configuration.Workflow != null)
+			{
+				foreach (WorkflowStepElement step in Configuration.Workflow)
+				{
+					if (step.BaseConfiguration.Element == null)
+						Children.Add(new ServiceDisplayInfo(step.ActualName, "(undefined base)"));
+					else
+						Children.Add(new ServiceDisplayInfo(step.BaseConfiguration.Element, step.ActualName));
+				}
+			}
+		}
+
+		public ServiceDisplayInfo(string name, string status)
+		{
+			Name = name;
+			_customStatus = status;
+		}
+
+		public void Start()
+		{
+			Start(Service.CreateInstance(Configuration));
+		}
+
+		// ...................
+		#endregion
+
+		#region internal
+		// ...................
+
+		void Start(ServiceInstance instance)
+		{
+			if (Instance != null)
+				throw new InvalidOperationException("Service is already started.");
+
+			AttachToInstance(instance);
+			Instance.Initialize();
+		}
+
+		void NotifyPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		void AttachToInstance(ServiceInstance instance)
+		{
+			Instance = instance;
+			Instance.StateChanged += new EventHandler<ServiceStateChangedEventArgs>(Instance_StateChanged);
+			Instance.OutcomeReported += new EventHandler(Instance_OutcomeReported);
+			Instance.ProgressReported += new EventHandler(Instance_ProgressReported);
+			Instance.ChildServiceRequested += new EventHandler<ServiceRequestedEventArgs>(Instance_ChildServiceRequested);
+
+			NotifyPropertyChanged("Status");
+			NotifyPropertyChanged("Progress");
+		}
+
+		void Instance_StateChanged(object sender, ServiceStateChangedEventArgs e)
+		{
+			NotifyPropertyChanged("Status");
+
+			if (e.StateAfter == ServiceState.Ready)
+			{
+				InstanceID = Instance.InstanceID;
+				NotifyPropertyChanged("InstanceID");
+				Instance.Start();
+			}
+		}
+
+		void Instance_OutcomeReported(object sender, EventArgs e)
+		{
+			NotifyPropertyChanged("Status");
+		}
+
+		void Instance_ProgressReported(object sender, EventArgs e)
+		{
+			NotifyPropertyChanged("Progress");
+		}
+
+		void Instance_ChildServiceRequested(object sender, ServiceRequestedEventArgs e)
+		{
+			ServiceDisplayInfo step = this.Children.Single(item => item.Name == e.ServiceName);
+			if (step != null)
+			{
+				step.Start(e.RequestedService);
+			}
+			else
+			{
+				step = new ServiceDisplayInfo(e.RequestedService.Configuration) { Unplanned = true };
+				step.Start(e.RequestedService);
+				this.Children.Add(step);
+			}
+		}
+
+		// ...................
+		#endregion 
+	}
+}
