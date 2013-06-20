09b83b4290187090df42cfd64310aa8f16cc162c
shirat <shirat@a563df95-700e-4ba9-babd-a50dda716297>
2013-06-20 12:08:37 +0000

    Esy Forex Backoffice: starting migration to DB3

---
+++ Applications/Edge.SDK.TestPipeline/Services/MyEasyForexBackofficeRetrieverService.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Edge.Core.Services;
+using Edge.Data.Pipeline.Services;
+
+namespace Edge.SDK.TestPipeline.Services
+{
+	public class MyEasyForexBackofficeRetrieverService : PipelineService
+	{
+		protected override ServiceOutcome DoPipelineWork()
+		{
+			foreach (var file in Delivery.Files)
+			{
+				file.Location = Path.Combine(@"C:\Development\Edge.bi\Files\EasyForex\BackofficeData", file.Name);
+			}
+			Delivery.Save();
+			return ServiceOutcome.Success;
+		}
+	}
+}
