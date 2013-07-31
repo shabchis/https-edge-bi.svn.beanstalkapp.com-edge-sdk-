ffd8358ba0f986252a8c61358e1f05a42bda5a51
shirat <shirat@a563df95-700e-4ba9-babd-a50dda716297>
2013-07-31 11:33:51 +0000

    Delivery Pipeline: add generic backoffice service + tests for Payoneer Backoffice

---
+++ Applications/Edge.SDK.TestPipeline/Services/MyBackofficeRetrieverService.cs
@@ -0,0 +1,24 @@
+﻿using Edge.Core.Services;
+using Edge.Data.Pipeline.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Edge.SDK.TestPipeline.Services
+{
+	public class MyBackofficeRetrieverService : PipelineService
+	{
+		protected override ServiceOutcome DoPipelineWork()
+		{
+			var dir = Configuration.Parameters.Get<string>("FileDirectory");
+			foreach (var file in Delivery.Files)
+			{
+				file.Location = Path.Combine(String.Format(@"C:\Development\Edge.bi\Files\Deliveries\{0}", dir), file.Name);
+			}
+			Delivery.Save();
+			return ServiceOutcome.Success;
+		}
+	}
+}
